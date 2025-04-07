db56d7cde3268f05b24a95ce2feadb7402cc8fc7
AbbasRostami <abbas.cracker@gmail.com>
2025-04-07 22:04:48 +0330

    add

---
+++ dist/assets/Sample.aa3ac853.js
@@ -0,0 +1,1 @@
+import{j as r,ai as a}from"./index.7784cd69.js";import{C as t,a as e}from"./App.0ffa3be7.js";const d=()=>r(t,{children:r(e,{children:r(a,{children:"Submit"})})});export{d as default};
