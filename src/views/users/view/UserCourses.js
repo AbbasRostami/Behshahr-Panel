17da5a973016c698d1b2ed90c74e21aa7b8cd131
AbbasRostami <abbas.cracker@gmail.com>
2025-04-10 02:11:58 +0330

    chore: remove unused project data and related imports from UserCourses.js

--- src/views/users/view/UserCourses.js
+++ src/views/users/view/UserCourses.js
@@ -2,83 +2,12 @@ import { Card } from "reactstrap";
 import { ChevronDown } from "react-feather";
 import DataTable from "react-data-table-component";
 import Avatar from "@components/avatar";
-import xdLabel from "@src/assets/images/icons/brands/xd-label.png";
-import vueLabel from "@src/assets/images/icons/brands/vue-label.png";
-import htmlLabel from "@src/assets/images/icons/brands/html-label.png";
-import reactLabel from "@src/assets/images/icons/brands/react-label.png";
-import sketchLabel from "@src/assets/images/icons/brands/sketch-label.png";
 import "@styles/react/libs/tables/react-dataTable-component.scss";
 import { useEffect, useState } from "react";
 import { getApi } from "../../../core/api/api";
 import { Cell } from "recharts";
 import { useParams } from "react-router-dom";
 
-const projectsArr = [
-  {
-    progress: 60,
-    hours: "2024/06/24",
-    progressColor: "info",
-    totalTasks: "233/240",
-    subtitle: "React Project",
-    title: "BGC eCommerce App",
-    img: reactLabel,
-  },
-  {
-    hours: "2024/06/24",
-    progress: 15,
-    totalTasks: "9/50",
-    progressColor: "danger",
-    subtitle: "UI/UX Project",
-    title: "Falcon Logo Design",
-    img: xdLabel,
-  },
-  {
-    progress: 90,
-    hours: "2024/06/24",
-    totalTasks: "100/190",
-    progressColor: "success",
-    subtitle: "Vuejs Project",
-    title: "Dashboard Design",
-    img: vueLabel,
-  },
-  {
-    hours: "2024/06/24",
-    progress: 49,
-    totalTasks: "12/86",
-    progressColor: "warning",
-    subtitle: "iPhone Project",
-    title: "Foodista mobile app",
-    img: sketchLabel,
-  },
-
-  {
-    progress: 73,
-    hours: "2024/06/24",
-    totalTasks: "234/378",
-    progressColor: "info",
-    subtitle: "React Project",
-    title: "Dojo React Project",
-    img: reactLabel,
-  },
-  {
-    progress: 81,
-    hours: "2024/06/24",
-    totalTasks: "264/537",
-    title: "HTML Project",
-    progressColor: "success",
-    subtitle: "Crypto Website",
-    img: htmlLabel,
-  },
-  {
-    progress: 78,
-    hours: "2024/06/24",
-    totalTasks: "214/627",
-    progressColor: "success",
-    subtitle: "Vuejs Project",
-    title: "Vue Admin template",
-    img: vueLabel,
-  },
-];
 
 export const columns = [
   {
