cdb542937a5a50b47b58e41fdfd3e994ecacc3dc
AbbasRostami <abbas.cracker@gmail.com>
2025-04-11 01:33:11 +0330

    chore: remove unnecessary comments in Router.js

--- src/router/Router.js
+++ src/router/Router.js
@@ -1,14 +1,10 @@
-// ** Router imports
 import { useRoutes } from "react-router-dom";
 
-// ** GetRoutes
 import { getRoutes } from "./routes";
 
-// ** Hooks Imports
 import { useLayout } from "@hooks/useLayout";
 
 const Router = () => {
-  // ** Hooks
   const { layout } = useLayout();
 
   const allRoutes = getRoutes(layout);
